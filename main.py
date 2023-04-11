9064337524e2e538122e51505fbf1e3ec2b05af2
Sumit Mukharjee <sumitmukharjeeeeee@gmail.com>
2023-04-11 23:21:53 +0530

    done til graphics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
 import cv2
-
 
 # Webcam
 cap = cv2.VideoCapture(0)
@@ -9,14 +9,24 @@ cap.set(4, 480)
 # Graphics
 imageBackground = cv2.imread('Resources/background.png')
 
+# Importing the mode images into the list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+    # print(imgModeList)
+
+# print(len(imgModeList))
+    # print(modePathList)
+
 
 while True:
     success, img = cap.read()
 
     # height and width points
-    imageBackground[162:162+480, 55:55+640] = img
-
+    imageBackground[162:162 + 480, 55:55 + 640] = img
+    imageBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
     # cv2.imshow("Webcam", img)
-
     cv2.imshow("Face Detection", imageBackground)
     cv2.waitKey(1)
