3aed55fd378f41331c5485696f83ed98c84ea893
Sumit Mukharjee <sumitmukharjeeeeee@gmail.com>
2023-04-13 10:47:37 +0530

    Firebase To Be Added

---
+++ AddDataToDatabase.py
@@ -0,0 +1,52 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://face-recognition-ba666-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "56567":
+        {
+            "name": "Robert Downey JR",
+            "major": "Artificial Intelligence",
+            "starting_year": 2017,
+            "total_attendance": 6,
+            "year": 4,
+            "last_attendance_time": "2024-04-13 00:54:34"
+        },
+    "56568":
+        {
+            "name": "Benedict Cumberbatch",
+            "major": "Surgeon",
+            "starting_year": 2018,
+            "total_attendance": 12,
+            "year": 5,
+            "last_attendance_time": "2023-04-13 00:54:34"
+        },
+    "56569":
+        {
+            "name": "Jennifer Lawrence",
+            "major": "Drama",
+            "starting_year": 2019,
+            "total_attendance": 11,
+            "year": 3,
+            "last_attendance_time": "2023-04-13 00:54:34"
+        },
+    "56571":
+        {
+            "name": "Shreyash Sinha",
+            "major": "Computer Science",
+            "starting_year": 2019,
+            "total_attendance": 26,
+            "year": 4,
+            "last_attendance_time": "2023-03-13 00:54:34"
+        },
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,6 +2,16 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://face-recognition-ba666-default-rtdb.firebaseio.com/",
+    'storageBucket': "face-recognition-ba666.appspot.com"
+})
 
 # Importing the person images
 folderPath = 'Images'
@@ -12,6 +22,12 @@ studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
     # print(path)
 # print(len(imgList))
 print(studentIds)
@@ -22,11 +38,12 @@ print(studentIds)
 def findEncodings(imgList):
     encodeList = []
     for img in imgList:
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
 
     return encodeList
+
 
 print("Encoding Started ...")
 encodeListKnown = findEncodings(imgList)
@@ -35,8 +52,7 @@ encodeListKnownWithIds = [encodeListKnown, studentIds]
 print("Encoding Complete!!")
 
 # saving file with picle
-file = open("EncodeFile.p",'wb') #open
-pickle.dump(encodeListKnownWithIds,file) #dump
-file.close() #close
+file = open("EncodeFile.p", 'wb')  # open
+pickle.dump(encodeListKnownWithIds, file)  # dump
+file.close()  # close
 print("File Saved")
-
