cbb9eada83bf4d54fff3b0af13a55d8d9b80e835
Sumit Mukharjee <sumitmukharjeeeeee@gmail.com>
2023-04-13 09:41:02 +0530

    Face Recognition Part done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
+import pickle
+
+import bbox as bbox
 import cv2
+import cvzone as cvzone
+import face_recognition
+import numpy as np
 
 # Webcam
 cap = cv2.VideoCapture(0)
@@ -18,15 +24,48 @@ for path in modePathList:
     # print(imgModeList)
 
 # print(len(imgModeList))
-    # print(modePathList)
+# print(modePathList)
 
+
+# Load the encoding file
+print("Loading Encoded File.....")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encoded File Loaded")
 
 while True:
     success, img = cap.read()
 
+    imgSmall = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgSmall)
+    encodeCurFrame = face_recognition.face_encodings(imgSmall, faceCurFrame)
+
     # height and width points
     imageBackground[162:162 + 480, 55:55 + 640] = img
     imageBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
-    # cv2.imshow("Webcam", img)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print("matches", matches)
+        print("faceDis", faceDis)
+
+        matchIndex = np.argmin(faceDis)
+        print("Match Index", matchIndex)
+
+        if matches[matchIndex]:
+            print("KNown Face Detected")
+            print(studentIds[matchIndex])
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            bbox = 55+x1, 162+y1, x2-x1, y2-y1
+            imageBackground = cvzone.cornerRect(imageBackground, bbox, rt=0)
+
+
+    # ncv2.imshow("Webcam", img)
     cv2.imshow("Face Detection", imageBackground)
     cv2.waitKey(1)
